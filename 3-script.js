1826a9014b9d5775cbb10bd6985496fc620bccac
Eag Udomsin <eagudomsin@hotmail.com>
2021-01-21 17:36:28 +0700

    first commit

---
+++ 3-script.js
@@ -0,0 +1,31 @@
+setInterval(function rescale() {
+var decorbox = $(".decor-box");
+var indexh = $(".index").height();
+var circle1 = $(".circle1");
+var circle2 = $(".circle2");
+
+decorbox.height(decorbox.width());
+circle1.height(circle1.width());
+circle2.height(circle2.width());
+
+$("#box5").css("top", 0.8 * indexh - decorbox.width());
+$("#box6").css("top", 0.8 * indexh - decorbox.width());
+$("#box7").css("top", 0.8 * indexh - decorbox.width());
+$("#box8").css("top", 0.8 * indexh - 2 * decorbox.width());
+$("#box9").css("top", 0.8 * indexh - 2 * decorbox.width());
+$("#box10").css("top", 0.8 * indexh - 3 * decorbox.width());
+$("#box11").css("top", - decorbox.width());
+$("#box12").css("top", - decorbox.width());
+$("#box12").css("left", decorbox.width());
+$("#box13").css("top", - 2 * decorbox.width());
+$("#box15").css("bottom", - decorbox.width());
+$("#box16").css("bottom", - 2 * decorbox.width());
+$("#box17").css("bottom",  0);
+$("#box18").css("bottom", - decorbox.width());
+$("#box18").css("left", decorbox.width());
+$("#box19").css("bottom", - decorbox.width());
+$("#box20").css("bottom", - 2 * decorbox.width());
+$("#box21").css("bottom",  0);
+$("#box22").css("bottom", - decorbox.width());
+$("#box22").css("right", decorbox.width());
+}, 500);
